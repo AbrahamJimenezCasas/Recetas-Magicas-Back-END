39b4bbd4824f8925dca0bf9afa1a449c57c77fa9
Abraham Jimenez <abrahamjimenezcasas@gmail.com>
2025-06-19 16:42:30 +0200

    Json y app js

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 // app.js (arranca el servidor)
-import server from "./src/server.js";
+import { server } from "./src/server.js";
 import { PORT } from "./env.js";
 
-const puerto = PORT;
+const puerto = PORT || 3001;
+
 server.listen(puerto, () => {
     console.log(`Servidor escuchando en el puerto: ${puerto}`);
 });
